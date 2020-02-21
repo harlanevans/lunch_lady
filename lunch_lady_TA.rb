0ea733e8de3d28547e9f794425d87d6c061a5cfa
Henry Doan <henrydoan96@gmail.com>
2020-02-20 18:33:01 -0700

    merge conflict

--- lunch_lady_TA.rb
+++ lunch_lady_TA.rb
@@ -1,12 +1,14 @@
 require 'pry'
 
 class MainDish
-  attr_accessor :item, :price
-  def initialize(item, price)
-    @item = item
-    @price = price
-    # @main_dish = []
-  end
+  <nav>
+  <ul>
+    <li><a href="#nowhere" title="Lorum ipsum dolor sit amet">Lorem</a></li>
+    <li><a href="#nowhere" title="Aliquam tincidunt mauris eu risus">Aliquam</a></li>
+    <li><a href="#nowhere" title="Morbi in sem quis dui placerat ornare">Morbi</a></li>
+    <li><a href="#nowhere" title="Praesent dapibus, neque id cursus faucibus">Praesent</a></li>
+    <li><a href="#nowhere" title="Pellentesque fermentum dolor">Pellentesque</a></li>
+  </ul>
 end
 
 class SideDish
